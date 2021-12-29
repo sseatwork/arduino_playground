57a07dcf71422f3b636663e7d641f06e9cf95caf
sseatwork <sseatwo@posteo.de>
2021-12-29 22:45:36 +0100

    some corrections

--- lcd/LcdSteering/LcdSteering.ino
+++ lcd/LcdSteering/LcdSteering.ino
@@ -5,6 +5,7 @@
 */
 
 // the setup function runs once when you press reset or power the board
+// ReSharper disable CppClangTidyClangDiagnosticSignCompare
 #include <LiquidCrystal.h>
 constexpr int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 constexpr int  switch_pin = 6;
@@ -13,9 +14,9 @@ int switchState = 0;
 int prevSwitchState = 0;
 int reply;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-String reihe = "                ";
-int satz_laenge;
-char zeichen;
+String empty_row = "                ";
+int string_length;
+char character_sel;
 void setup() {
 	lcd.begin(16, 2);
 	pinMode(switch_pin, INPUT);
@@ -23,21 +24,21 @@ void setup() {
 	lcd.setCursor(0, 1);
 	lcd.print("Kritsallkugel");
 }
-void printMarquee(int line, String iLine, LiquidCrystal lcd)
+void printMarquee(const int line, const String& i_line, LiquidCrystal internal_lcd)
 {
 
-	for (int i = 0; i < iLine.length() + 16; i++) {
+	for (int i = 0; i < i_line.length() + 16; i++) {
 		for (int j = 0; j < 15; j++) {
-			zeichen = reihe.charAt(j + 1);
-			reihe.setCharAt(j, zeichen);
+			character_sel = empty_row.charAt(j + 1);
+			empty_row.setCharAt(j, character_sel);
 		}
-		zeichen = ' ';
-		if (i < iLine.length()) {
-			zeichen = iLine.charAt(i);
+		character_sel = ' ';
+		if (i < i_line.length()) {
+			character_sel = i_line.charAt(i);
 		}
-		reihe.setCharAt(15, zeichen);
-		lcd.setCursor(0, line);
-		lcd.print(reihe);
+		empty_row.setCharAt(15, character_sel);
+		internal_lcd.setCursor(0, line);
+		internal_lcd.print(empty_row);
 		delay(250);
 	}
 }
@@ -52,18 +53,18 @@ void loop() {
 			lcd.clear();
 			lcd.noAutoscroll();
 			lcd.setCursor(0, 0);
-			lcd.print("Die Kugel sagt:");
+			lcd.print("Wer sagt:");
 			switch (reply)
 			{
 			case 0:
 
-				printMarquee(1, "ja", lcd);
+				printMarquee(1, "Ja", lcd);
 				break;
 			case 1:
-				printMarquee(1, "gaaanz bestimmt", lcd);
+				printMarquee(1, "Gaaanz bestimmt", lcd);
 				break;
 			case 2:
-				printMarquee(1, "Klar", lcd);
+				printMarquee(1, "Klar doch", lcd);
 				break;
 			case 3:
 				printMarquee(1, "Ohh nooo", lcd);
@@ -72,13 +73,13 @@ void loop() {
 				printMarquee(1, "Unklar", lcd);
 				break;
 			case 5:
-				printMarquee(1, "verstehe ich nich", lcd);
+				printMarquee(1, "Verstehe ich nich...", lcd);
 				break;
 			case 6:
-				printMarquee(1, "wer will das wissen", lcd);
+				printMarquee(1, "Wer will das wissen", lcd);
 				break;
 			case 7:
-				printMarquee(1, "Niemals", lcd);
+				printMarquee(1, "Niemals, no never ever", lcd);
 				break;
 			}
 		}
